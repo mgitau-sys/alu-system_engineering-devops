6d1b6c7ce838c60f8ef472e033217593a8b412b4
mgitau-sys <m.gitau@alustudent.com>
2026-07-18 12:29:44 +0200

    adding comment

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,11 @@
+#This puppet is making sure that our config file doesn't allow passwords and uses ~/.ssh/school as our ssh key
 file_line { 'Declare identity file':
   ensure => present,
   path   => 'home/ubuntu/.ssh/config',
   line   => '    IdentityFile ~/.ssh/school',
 }
 
-File_line { 'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => 'home/ubuntu/.ssh/config',
   line   => '    PasswordAuthentication no',
