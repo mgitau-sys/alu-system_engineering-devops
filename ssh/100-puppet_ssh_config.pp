76928f990a79ccbc0e06c8ba5f630ca877ef7fc3
mgitau-sys <m.gitau@alustudent.com>
2026-07-18 12:32:34 +0200

    correcting file path

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #This puppet is making sure that our config file doesn't allow passwords and uses ~/.ssh/school as our ssh key
 file_line { 'Declare identity file':
   ensure => present,
-  path   => 'home/ubuntu/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => 'home/ubuntu/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
